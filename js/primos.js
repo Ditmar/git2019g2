e35303b44b412b663ace396e079a04f2c5356f65
pepe@gmail.com <Ditmar>
2019-03-27 09:26:53 -0400

    multiplicacion matricial y ecuacion

--- js/primos.js
+++ js/primos.js
@@ -9,3 +9,28 @@ function primos(numbers) {
   }
   return true;
 }
+function mulMatrix (matrix1, matrix2) {
+  var matrixresult = [];
+  for (var i = 0; i < matrix1.length; i ++) {
+    var auxarr = [];
+    for (var j = 0; j < matrix1.length; j ++) {
+      var mul = 0;
+      for (var k = 0; k < matrix1.length; k ++) {
+        mul += matrix1[j][k] * matrix2[k][j];
+      }
+      auxarr.push(mul);
+    }
+    matrixresult.push(auxarr);
+  }
+  return matrixresult;
+}
+function solveEcuation(a, b, c) {
+  var dist = b*b - 4 * a * c;
+  if (dist == 0) {
+    return  {result: -b / 2 * a, "msn" : "Sol iguales"};
+  } else if (dist < 0)  {
+    return  {"msn" : "Sol Compleja"};
+  } else {
+    return  {x1: (-b + Math.sqrt(dist)) / 2 * a, x2: (-b - Math.sqrt(dist)) / 2 * a,  "msn" : "Sol Compleja"};
+  }
+}
